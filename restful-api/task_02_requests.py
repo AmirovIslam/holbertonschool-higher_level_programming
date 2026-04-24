4ff1eb4edbf058251d04e1a5e56ef213bb5b72de
AmirovIslam <13599@holbertonstudents.com>
2026-04-24 22:20:57 +0000

    Add restful-api tasks

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,19 @@
+import requests
+import csv
+
+def fetch_and_print_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print("Status Code: {}".format(response.status_code))
+    if response.status_code == 200:
+        for post in response.json():
+            print(post.get("title"))
+
+def fetch_and_save_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    if response.status_code == 200:
+        posts = response.json()
+        data = [{"id": post.get("id"), "title": post.get("title"), "body": post.get("body")} for post in posts]
+        with open("posts.csv", "w", newline="", encoding="utf-8") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(data)
